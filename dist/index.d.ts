87c2f0148be801e03fe9b5d3db5d70586688346a
Error <foamy125@gmail.com>
2025-11-08 20:43:04 -0600

    smart-commit success Explanation: n

---
+++ dist/index.d.ts
@@ -0,0 +1,3 @@
+export * from "./rules.js";
+export * from "./rewrite.js";
+//# sourceMappingURL=index.d.ts.map
---
+++ dist/rules.d.ts
@@ -0,0 +1,4 @@
+export declare const TOPIC_RE: RegExp;
+export declare function isValidTopic(t: string): boolean;
+export declare function headerOk(h: string): boolean;
+//# sourceMappingURL=rules.d.ts.map
